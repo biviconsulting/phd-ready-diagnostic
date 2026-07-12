c47dc84fc157836d07d0cfa75b9f24c545b32763
biviconsulting <bivi215@gmail.com>
2026-07-11 19:11:34 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@iframe-resizer/child';
 import PhDReadyDiagnostic from './PhDReadyDiagnostic.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
